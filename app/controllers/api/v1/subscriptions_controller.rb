a15acea14996c6fd7cd745d8112526175a70ffb2
Matt Holmes <78521446+matthewjholmes@users.noreply.github.com>
2022-02-24 08:57:40 -0700

    Add functionality and tests for updating (cancelling) subscription.

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -9,6 +9,12 @@ class Api::V1::SubscriptionsController < ApplicationController
     end
   end
 
+  def update
+    subscription = Subscription.find(params[:id])
+    subscription.update(status: params[:status])
+    render json: { response: 'Subscription has been cancelled.'}, status: :ok
+  end
+
   private
 
   def subscription_params
