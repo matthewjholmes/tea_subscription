ab08a933e0b7683e002b2dc8b8e95d09e65450a7
Matt Holmes <78521446+matthewjholmes@users.noreply.github.com>
2022-02-24 09:57:27 -0700

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Subscription.destroy_all
+Customer.destroy_all
+Tea.destroy_all
+
+Customer.create!(first_name: 'Brian', last_name: 'Zanti', email: 'brian@email.com', address: '12345 Pwahoo Ln., Arvada CO 80001')
+Customer.create!(first_name: 'James', last_name: 'Baker', email: 'james@email.com', address: '135 Jordan Way, Arvada CO 80001')
+
+Tea.create!(title: 'Tea 1', description: 'A real nice beverage.', temperature: '201', brew_time: '2')
+Tea.create!(title: 'Tea 2', description: 'A real nice beverage.', temperature: '202', brew_time: '2')
+Tea.create!(title: 'Tea 3', description: 'A real nice beverage.', temperature: '203', brew_time: '2')
